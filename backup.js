353981f842250bba4c83ea13c2aef63e00aef82a
lavimonteiro <lavinia.monteiro@stileeducation.com>
2022-04-22 13:25:46 +1000

    started organising code

--- backup.js
+++ backup.js
@@ -1,23 +1,28 @@
 
-document.addEventListener("DOMContentLoaded", () => {
-const loginForm = document.querySelector('#login');
-const createAccountForm = document.querySelector('#createAccount');
+function loginForm() {
+    return document.forms["login"];
+};
 
+function createAccountForm() {
+    return document.forms["createAccount"];
+}
+
+function toggleCreateAccountLoginForms () {
     document.querySelector("#linkCreateAccount").addEventListener("click", e=> {
     e.preventDefault();
-    loginForm.classList.add("form--hidden")
-    createAccountForm.classList.remove("form--hidden")
+    loginForm().classList.add("form--hidden")
+    createAccountForm().classList.remove("form--hidden")
     });
 
     document.querySelector("#linkLogin").addEventListener("click", e=>{
     e.preventDefault();
-    loginForm.classList.remove("form--hidden")
-    createAccountForm.classList.add("form--hidden")
+    loginForm().classList.remove("form--hidden")
+    createAccountForm().classList.add("form--hidden")
     });
-});
+}
 
-document.addEventListener("DOMContentLoaded", () =>{
-    document.forms["createAccount"].addEventListener("submit", (event) => {
+function storingCreatAccountDetails(){
+    createAccountForm().addEventListener("submit", (event) => {
         event.preventDefault();
 
         let  inputs = document.querySelectorAll("#createAccount input");
@@ -34,43 +39,45 @@ document.addEventListener("DOMContentLoaded", () =>{
                     localStorage.setItem(input.name, input.value);
                 });
 
-                const loginForm = document.querySelector('#login');
-                const createAccountForm = document.querySelector('#createAccount');
-                loginForm.classList.remove("form--hidden")
-                createAccountForm.classList.add("form--hidden")
+                loginForm().classList.remove("form--hidden")
+                createAccountForm().classList.add("form--hidden")
                 console.log(localStorage)
+
+                const usernameInput = document.getElementsByName("username");
+                console.log(usernameInput, usernameInput.value)
+
             }
             else {
             let messageElement = document.querySelector(".create-account-error")
             messageElement.textContent = "Password does not match. Please Try again."
             }
         })
-    });
+}
 
-document.addEventListener("DOMContentLoaded", () =>{
+document.addEventListener("DOMContentLoaded", toggleCreateAccountLoginForms);
+document.addEventListener("DOMContentLoaded", storingCreatAccountDetails);
+
+
+    document.addEventListener("DOMContentLoaded", () =>{
     document.forms["login"].addEventListener("submit", (event) => {
         event.preventDefault();
-
         let informationArray = [];
-
         let  inputs = document.querySelectorAll("#login input");
         inputs.forEach( input => {
             informationArray.push(input.value);
             });
 
         if (localStorage.getItem("username") === informationArray[0] && localStorage.getItem("password") === informationArray[1]){
-            console.log("success")
             let messageElement = document.querySelector(".login-error")
             messageElement.textContent = ""
 
             sessionStorage.setItem("login successful", localStorage.getItem("username"))
             console.log(sessionStorage)
 
-            const loginForm = document.querySelector('#login');
-            loginForm.classList.add("form--hidden")
+            loginForm().classList.add("form--hidden")
+            const logoutForm = document.querySelector('#logout');
+            logoutForm.classList.remove("form--hidden")
 
-            let container = document.querySelector(".profile-container")
-            container.textContent = "Hello " + localStorage.getItem("username") + " you're logged in!"
         }
 
         else {
@@ -80,12 +87,27 @@ document.addEventListener("DOMContentLoaded", () =>{
     });
 });
 
-// document.addEventListener("DOMContentLoaded", () =>{
-// if (sessionStorage){
-//     const loginForm = document.querySelector('#login');
-//     loginForm.classList.add("form--hidden")
+document.addEventListener("DOMContentLoaded", () =>{
+//what should i check for??
+    if (sessionStorage.length > 1){
+        loginForm().classList.add("form--hidden")
+            const logoutForm = document.querySelector('#logout');
+            logoutForm.classList.remove("form--hidden")
 
-//     let container = document.querySelector(".profile-container")
-//     container.textContent = "Hello " + localStorage.getItem("username") + " you're logged in!"
-//     }
-// });
+
+
+        document.forms["logout"].addEventListener("submit", e =>{
+            e.preventDefault();
+            sessionStorage.clear();
+
+            const loginForm = document.querySelector('#login');
+            loginForm.classList.remove("form--hidden")
+            const logoutForm = document.querySelector('#logout');
+            logoutForm.classList.add("form--hidden")
+        });
+    }
+
+});
+
+
+//everything works but code is not fully organised yet 
